2ca90d691757bc3426e3a7ad1ef9b3955e940695
ivankopanyov <i.kopanyov@gmail.com>
2022-08-19 10:20:56 +0300

    Добавлено решение задачи №3 ДЗ №2

---
+++ Homework2/Task3.py
@@ -0,0 +1,25 @@
+# Задача 3. Реализуйте алгоритм перемешивания списка.
+
+import random
+
+# Перемешивание списка
+def shuffle_list(value):
+    if type(value) != list:
+        return None
+    if len(value) <= 1:
+        return value
+    for i in range(0, len(value) - 1):
+        temp = value[i]
+        rand_num = random.randint(i + 1, len(value) - 1)
+        value[i] = value[rand_num]
+        value[rand_num] = temp
+    return value
+
+print('Перемешивание списка')
+print('Для выхода оставьте поле пустым')
+
+while True:
+    s = input('Введите элементы списка через пробел: ')
+    if s == '':
+        exit()
+    print(*shuffle_list(s.split()))
